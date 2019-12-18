22d58e45c3c2b3e2c7c0e64b90bd318258929cd6
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-12-18 05:09:17 -0600

    added component framework files needed for application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import BoxList from './BoxList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Something...</h1>
+      <BoxList/>
     </div>
   );
 }
---
+++ src/BoxList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import NewBoxForm from './NewBoxForm';
+import Box from './Box';
+
+
+class BoxList extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Hello!</h1>
+            </div>
+        )
+    }
+}
+
+export default BoxList;
+
+
+
+
+
